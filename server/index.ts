e00fc6d047fa129410347b90bebc403d0417d25f
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2025-08-06 07:03:55 -0600

    feat: database connected and functional

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,33 @@
+import dotenv from "dotenv"
+dotenv.config();
+
+import pkg from "pg";
+const { Pool } = pkg;
+
 import cors from "cors";
 import createServer from "./utils/server.js";
 
 const app = createServer();
 
 app.use(cors());
-const port = 3000;
+const port = process.env.PORT || 3001;
 
-const startServer = () => {
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default pool;
+
+const startServer = async () => {
   try {
+    await pool.connect();
+    console.log("Connected to the database successfully");
+
     app.listen(port, () => {
       console.log(`Server listening on http://localhost:${port}`);
     });
   } catch (error) {
-    console.error("Error starting server:", error);
+    console.error("Unable to connect to database:", error);
   }
 };
 
--- server/utils/server.ts
+++ server/utils/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import lobbyRouter from "../controllers/lobby.js";
+import oddOneOutRouter from "../controllers/oddOneOut.js";
 
 const createServer = () => {
   const app = express();
@@ -11,6 +12,7 @@ const createServer = () => {
   app.use(cors());
 
   app.use("/api/lobby", lobbyRouter);
+  app.use("/api/oddoneout", oddOneOutRouter);
 
   app.get("/", (req, res) => {
     res.send("Hello from backend!");
